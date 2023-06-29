36243bbf0a727ca4d36104935571cd2a80be6eea
Paulette Zaldivar Flores <paulettz@icloud.com>
2023-06-29 21:22:10 +0900

    added avatar to user

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,30 +1,39 @@
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import './Navbar.css';
 import logo from './images/logo.png';
+import avatar from './images/avatar.png'
 
 
+const MovieNavbar = ({ current_user, userSignOut }) => {
+  const isLoggedIn = current_user !== null;
 
-function MovieNavbar() {
   return (
-    <Navbar className="custom-navbar" variant="light" expand="dark">
+    <Navbar className="custom-navbar me-auto" variant="light" expand="lg">
       <Container>
-        <Navbar.Brand href="/"><img
-              src={logo}
-              alt = "logo"
-              className = "logo"
-            /></Navbar.Brand>
-        <Navbar.Toggle className = "navlink" aria-controls="basic-navbar-nav" />
+        <Navbar.Brand href="/">
+          <img src={logo} alt="logo" className="logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle className="navlink" aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link className = "navlink" href="/">Home</Nav.Link>
-            <Nav.Link className = "navlink" href="/Watch-Next">Watch Next</Nav.Link>
+            <Nav.Link className="navlink" href="/">Home</Nav.Link>
+            <Nav.Link className="navlink" href="/Watch-Next">Watch Next</Nav.Link>
           </Nav>
+          {isLoggedIn && (
+            <Nav>
+              <NavDropdown title={<img src={avatar} alt="Avatar" className="avatar" />} id="nav-dropdown">
+                <NavDropdown.Item onClick={userSignOut} href={`user_path(${current_user})`}>Log Out</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-}
+};
 
 export default MovieNavbar;
